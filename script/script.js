5ccd4bbe418c3100fb957703adc9245bea2fa68e
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-03-05 14:03:58 +0700

    Menambahkan Folder video

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// let nama = prompt("Masukkan Nama");
+// let age = prompt("Masukkan Umur");
+// let arraykosong = [];`
+
+// arraykosong.push(nama, parseInt(age));
+
+// if (isNaN(arraykosong[1])) {
+//     console.log('Mohon Untuk Hanya Memasukkan Angka');
+// } else {
+//     arraykosong[0] = 'Sultan Faiz Mochammad'
+//     console.log(`Nama Saya ${arraykosong[0]}. Saya Berumur ${arraykosong[1]} Tahun`);
+// }
