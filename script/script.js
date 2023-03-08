37feefa774f40e39dd699afcdca6f8964740d1d2
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-03-08 15:59:55 +0700

    Mengubah Judul Website menjadi My Portofolio

--- script/script.js
+++ script/script.js
@@ -1,14 +1,3 @@
-// let nama = prompt("Masukkan Nama");
-// let input = prompt("Masukkan Umur");
-// let umur = parseInt(input);
-// let obj = {
-//     nama: nama,
-//     umur: umur 
-// };
+// Mengecek Apakah Variable info bernilai NULL atau Undefined
 
-// if (isNaN(obj.umur)) {
-//     console.log('Mohon untuk memasukkan tipe data INTERGER');
-// } else {
-//     obj.nama = 'Sultan Faiz Mochammad'
-//     console.log(`Nama Saya ${obj.nama}. Saya Berumur ${obj.umur} tahun`);
-// }
+const button = 
