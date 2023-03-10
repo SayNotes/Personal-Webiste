42c6a8056185abeee5d38ebf5c3bc70391798194
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-03-10 15:04:08 +0700

    Menambahkan data diri singkat di about-page dan membuat javascript menghitung umur

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
+const tahun = new Date();
+let birthday = new Date('June 2, 2005');
+let now = tahun.getFullYear() - birthday.getFullYear();
 
+if ( tahun.getMonth() < birthday.getMonth() ||
+    (tahun.getMonth() == birthday.getMonth() &&
+    tahun.getDate() < birthday.getDate())
+) {
+    now--;
+};
+document.querySelector('#age').innerHTML = `${now} Tahun`;
