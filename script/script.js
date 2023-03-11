35aace68d851fd6d28803534296ab42a3c92d6a5
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-03-11 15:57:02 +0700

    Menambahkan style jam di navbar

--- script/script.js
+++ script/script.js
@@ -1,34 +1,39 @@
 const dates = new Date();
-let birthday = new Date('June 2, 2005');
+let birthday = new Date("June 2, 2005");
 let age = dates.getFullYear() - birthday.getFullYear();
 
-if ( dates.getMonth() < birthday.getMonth() ||
+if (
+    dates.getMonth() < birthday.getMonth() ||
     (dates.getMonth() == birthday.getMonth() &&
-    dates.getDate() < birthday.getDate())
+        dates.getDate() < birthday.getDate())
 ) {
     age--;
-};
+}
 
-document.querySelector('#age').innerHTML = `${age} Tahun`;
+document.querySelector("#age").innerHTML = `${age} Tahun`;
 
 // membuat jam
+
+let clockHours = document.getElementById("hours");
+let clockMinutes = document.getElementById("minutes");
 
 let clock = {
     jam: dates.getHours(),
     menit: dates.getMinutes(),
-    detik: dates.getSeconds()
+    detik: dates.getSeconds(),
 };
 
-let timerInterval = setInterval(function() {
+let timerInterval = setInterval(function () {
     clock.detik++;
-    console.log(clock)
     if (clock.detik >= 60) {
         clock.detik = 0;
-        clock.menit++
+        clock.menit++;
     } else if (clock.menit >= 60) {
         clock.menit = 0;
-        clock.jam++
+        clock.jam++;
     } else if (clock.jam >= 24) {
-        clock.jam = 0
+        clock.jam = 0;
     }
+    clockHours.textContent = clock.jam.toString();
+    clockMinutes.textContent = clock.menit.toString().padStart(2, '0');
 }, 1000);
