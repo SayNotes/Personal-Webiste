e9a833ba7297fb13f77a833b9f94493ce727fd78
Sultan Faiz Mochammad <sultangrowsgt@gmail.com>
2023-03-11 13:27:26 +0700

    Menambahkan Jam dengan javascript

--- script/script.js
+++ script/script.js
@@ -1,11 +1,34 @@
-const tahun = new Date();
+const dates = new Date();
 let birthday = new Date('June 2, 2005');
-let now = tahun.getFullYear() - birthday.getFullYear();
+let age = dates.getFullYear() - birthday.getFullYear();
 
-if ( tahun.getMonth() < birthday.getMonth() ||
-    (tahun.getMonth() == birthday.getMonth() &&
-    tahun.getDate() < birthday.getDate())
+if ( dates.getMonth() < birthday.getMonth() ||
+    (dates.getMonth() == birthday.getMonth() &&
+    dates.getDate() < birthday.getDate())
 ) {
-    now--;
+    age--;
 };
-document.querySelector('#age').innerHTML = `${now} Tahun`;
+
+document.querySelector('#age').innerHTML = `${age} Tahun`;
+
+// membuat jam
+
+let clock = {
+    jam: dates.getHours(),
+    menit: dates.getMinutes(),
+    detik: dates.getSeconds()
+};
+
+let timerInterval = setInterval(function() {
+    clock.detik++;
+    console.log(clock)
+    if (clock.detik >= 60) {
+        clock.detik = 0;
+        clock.menit++
+    } else if (clock.menit >= 60) {
+        clock.menit = 0;
+        clock.jam++
+    } else if (clock.jam >= 24) {
+        clock.jam = 0
+    }
+}, 1000);
